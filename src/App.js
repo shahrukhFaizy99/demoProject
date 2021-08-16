7652d1ad8c01e72ef5285d5ef3217b183c5edb11
shahrukhFaizy99 <shahrukhfaizy17@gmail.com>
2021-08-16 17:58:02 +0500

    2ndCommit

--- src/App.js
+++ src/App.js
@@ -11,12 +11,59 @@ const App = () => {
       <Carousel />
       <h1 className="h1">CARDS SECTION</h1>
       <div className="flex">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          img="/images/dark.JPG"
+          alt=""
+          width="150"
+          height="80"
+          title="DARK"
+          text="NETFLIX SERIES"
+        />
+
+        <Card
+          img="/images/stranger.JPG"
+          alt=""
+          width="150"
+          height="80"
+          title="STRANGER THINGS"
+          text="NETFLIX SERIES"
+        />
+
+        <Card
+          img="/images/money-heist.JPG"
+          alt=""
+          width="150"
+          height="80"
+          title="MONEY HEIST"
+          text="NETFLIX SERIES"
+        />
+
+        <Card
+          img="/images/extraction.JPG"
+          alt=""
+          width="150"
+          height="80"
+          title="Extraction"
+          text="NETFLIX SERIES"
+        />
+
+        <Card
+          img="/images/sacred-games.JPG"
+          alt=""
+          width="150"
+          height="80"
+          title="SACRED GAMES"
+          text="NETFLIX SERIES"
+        />
+
+        <Card
+          img="/images/mirzapur.JPG"
+          alt=""
+          width="150"
+          height="80"
+          title="MIRZAPUR"
+          text="NETFLIX SERIES"
+        />
       </div>
       <Modal />
     </>
--- src/components/card.js
+++ src/components/card.js
@@ -1,24 +1,19 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <>
       <div className="card">
-        <img
-          className="card-img"
-          src="/images/birthday.JPG"
-          alt=""
-          width="150"
-          height="80"
-        ></img>
+        <img className="card-img" src={props.img} />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a className="btnCheckIn" href="#" className="btn btn-primary">
-            CHECK IN
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.text}</p>
+          <a
+            className="btnCheckIn"
+            href="https://www.youtube.com/watch?v=rrwycJ08PSA"
+            className="btn btn-primary"
+          >
+            WATCH NOW
           </a>
         </div>
       </div>
